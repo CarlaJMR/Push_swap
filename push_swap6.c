80707c43421b50be11eab7416a89878d5ce7e688
CarlaJMR <116879416+CarlaJMR@users.noreply.github.com>
2023-04-21 17:31:09 +0100

    Add files via upload

--- push_swap6.c
+++ push_swap6.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   push_swap6.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cjoao-me <cjoao-me@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 09:53:10 by cjoao-me          #+#    #+#             */
-/*   Updated: 2023/04/21 17:29:11 by cjoao-me         ###   ########.fr       */
+/*   Updated: 2023/04/21 10:58:08 by cjoao-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,6 +90,20 @@ int	ft_lstminimum(t_list *lst)
 	return (min);
 }
 
+int	ft_lstmaximum(t_list *lst)
+{
+	int	max;
+
+	max = lst->content;
+	while (lst)
+	{
+		if (max < lst->content)
+			max = lst->content;
+		lst = (*lst).next;
+	}
+	return (max);
+}
+
 int	ft_lstposition(int n, t_list *lst)
 {
 	int	p;
@@ -105,7 +119,7 @@ int	ft_lstposition(int n, t_list *lst)
 
 int	ft_lstsorted(t_list *lst, char l)
 {
-	if (l == 'a')
+	if (l = 'a')
 	{
 		while (lst && (*lst).next)
 		{
@@ -113,8 +127,9 @@ int	ft_lstsorted(t_list *lst, char l)
 				return (0);
 			lst = (*lst).next;
 		}
+		return (1);
 	}
-	else
+	else if (l = 'b')
 	{
 		while (lst && (*lst).next)
 		{
@@ -122,6 +137,30 @@ int	ft_lstsorted(t_list *lst, char l)
 				return (0);
 			lst = (*lst).next;
 		}
+		return (1);
+	}
+
+}
+
+int	ft_lstsorted_a(t_list *lst)
+{
+
+	while (lst && (*lst).next)
+	{
+		if (lst->content > (*lst).next->content)
+			return (0);
+		lst = (*lst).next;
+	}
+	return (1);
+}
+
+int	ft_lstsorted_b(t_list *lst)
+{
+	while (lst && (*lst).next)
+	{
+		if (lst->content < (*lst).next->content)
+			return (0);
+		lst = (*lst).next;
 	}
 	return (1);
 }
@@ -203,7 +242,7 @@ int	check_arg_order(int argc, char **argv)
 	return (0);
 }
 
-void	move_s(t_list	**stack, char s)
+void	move_sa(t_list	**stack)
 {
 	t_list	*temp;
 
@@ -211,13 +250,21 @@ void	move_s(t_list	**stack, char s)
 	*stack = (*stack)->next;
 	temp->next = (*stack)->next;
 	(*stack)->next = temp;
-	if (s == 'a')
-		write(1,"sa\n", 3);
-	else if (s == 'b')
-		write(1,"sb\n", 3);
+	write(1,"sa\n", 3);
 }
 
-void	move_r(t_list	**stack, char s)
+void	move_sb(t_list	**stack)
+{
+	t_list	*temp;
+
+	temp = *stack;
+	*stack = (*stack)->next;
+	temp->next = (*stack)->next;
+	(*stack)->next = temp;
+	write(1,"sb\n", 3);
+}
+
+void	move_ra(t_list	**stack)
 {
 	t_list	*temp_1;
 	t_list	*temp_2;
@@ -231,13 +278,26 @@ void	move_r(t_list	**stack, char s)
 	*stack = (*stack)->next;
 	temp_1->next = temp_2;
 	temp_2->next = NULL;
-	if (s == 'a')
-		write(1,"ra\n", 3);
-	else if (s == 'b')
-		write(1,"rb\n", 3);
+	write(1,"ra\n", 3);
 }
 
-void	move_rr(t_list **stack, char s)
+void	move_rb(t_list	**stack)
+{
+	t_list	*temp_1;
+	t_list	*temp_2;
+
+	temp_1 = *stack;
+	while (temp_1->next != NULL)
+	{
+		temp_1 = temp_1->next;
+	}
+	temp_2 = *stack;
+	*stack = (*stack)->next;
+	temp_1->next = temp_2;
+	temp_2->next = NULL;
+	write(1,"rb\n", 3);
+}
+void	move_rra(t_list **stack)
 {
 	t_list	*temp;
 	t_list	*new_end;
@@ -250,12 +310,24 @@ void	move_rr(t_list **stack, char s)
 		new_end = new_end->next;
 	new_end->next = NULL;
 	*stack = temp;
-	if (s == 'a')
-		write(1,"rra\n", 4);
-	else if (s == 'b')
-		write(1,"rrb\n", 4);
+	write(1,"rra\n", 4);
 }
 
+void	move_rrb(t_list **stack)
+{
+	t_list	*temp;
+	t_list	*new_end;
+
+	temp = *stack;
+	temp = ft_lstlast(temp);
+	temp->next = *stack;
+	new_end = *stack;
+	while (new_end->next != temp)
+		new_end = new_end->next;
+	new_end->next = NULL;
+	*stack = temp;
+	write(1,"rrb\n", 4);
+}
 void	move_pa(t_list	**stack_a, t_list	**stack_b)
 {
 	t_list	*temp;
@@ -282,6 +354,7 @@ void	move_pb(t_list	**stack_a, t_list	**stack_b)
 	write(1, "pb\n", 3);
 }
 
+
 void	sort_3(t_list **stack_a)
 {
 	int	a;
@@ -293,19 +366,20 @@ void	sort_3(t_list **stack_a)
 	c = (*stack_a)->next->next->content;
 	if (a > b && a > c)
 	{
-		move_r(stack_a, 'a');
+		move_ra(stack_a);
 		if (b > c)
-		move_s(stack_a, 'a');
+		move_sa(stack_a);
 	}
 	else if (b > a && b > c)
 	{
-		move_rr(stack_a, 'a');
+		move_rra(stack_a);
 		if (a < c)
-		move_s(stack_a, 'a');
+		move_sa(stack_a);
 	}
 	else
-		move_s(stack_a, 'a');
+		move_sa(stack_a);
 }
+
 
 void	pull_number_up(t_list **stack_a, int nbr)
 {
@@ -318,15 +392,34 @@ void	pull_number_up(t_list **stack_a, int nbr)
 	i = 0;
 	if (pos <= len /2 + 1)
 		while(++i < pos)
-			move_r(stack_a, 'a');
+			move_ra(stack_a);
 	else
 		while(len - i >= pos)
 		{
-			move_rr(stack_a, 'a');
+			move_rra(stack_a);
 			i++;
 		}
 }
 
+void	pull_number_up_b(t_list **stack_b, int nbr)
+{
+	int	len;
+	int	pos;
+	int	i;
+
+	len = ft_lstsize(*stack_b);
+	pos = ft_lstposition(nbr, *stack_b);
+	i = 0;
+	if (pos <= len /2 + 1)
+		while(++i < pos)
+			move_rb(stack_b);
+	else
+		while(len - i >= pos)
+		{
+			move_rrb(stack_b);
+			i++;
+		}
+}
 void	sort_4_5(t_list **stack_a, t_list **stack_b)
 {
 	int	len;
@@ -356,12 +449,18 @@ int	get_pivot(t_list *lst, int size)
 	int	i, j;
 	int	temp;
 
+	//if (size < 3)
+		//EXIT_FAILURE;
 	i= 0;
 	while (i < size)
 	{
 		arr[i++] = lst->content;
 		lst = (*lst).next;
 	}
+	/*i = 0;
+	while (i < size)
+		printf("%i ", arr[i++]);
+		printf("\n");*/
 	i = 0;
 	temp = arr[0];
 	while (i< size)
@@ -379,79 +478,79 @@ int	get_pivot(t_list *lst, int size)
 		}
 		i++;
 	}
+	/*i= 0;
+	while (i < size)
+		printf("%i ", arr[i++]);
+		printf("\n");*/
 	return (arr[size / 2]);
 }
 
-void	sort_three_top_a(t_list **stack, int size)
+/*void	partition_smaller(t_list **stack_a, t_list **stack_b)
 {
-	int	a;
-	int	b;
-	int	c;
-	
-	if (size == 2 && (*stack)->content > (*stack)->next->content)
-		move_s(stack, 'a');
-	if (size == 3)
+	int	i;
+	int	len;
+	int	pivot;
+
+	len = ft_lstsize(*stack_a);
+	//printf("len = %i\n", len);
+	pivot = get_pivot(*stack_a, ft_lstsize(*stack_a));
+	//printf("%i\n", pivot);
+	i = 0;
+	while (i < len && ft_lstsize(*stack_a) > 5)
 	{
-		a = (*stack)->content;
-		b = (*stack)->next->content;
-		c = (*stack)->next->next->content;
-		if (a < b && b > c)
-		{
-			move_r(stack, 'a');
-			move_s(stack, 'a');
-			move_rr(stack, 'a');
-			if (a > c)
-				move_s(stack, 'a');
-		}
-		else if (a > b && a > c)
-		{
-			move_s(stack, 'a');
-			move_r(stack, 'a');
-			move_s(stack, 'a');
-			move_rr(stack, 'a');
-			if (b > c)
-				move_s(stack, 'a');
-		}
+		if ((*stack_a)->content <= pivot && ft_lstsize(*stack_a) > 5)
+			move_pb(stack_a, stack_b);
 		else
-			move_s(stack, 'a');
+			move_ra(stack_a);
+		i++;
 	}
 }
 
-void	sort_three_top_b(t_list **stack, int size)
+void	partition_bigger(t_list **stack_a, t_list **stack_b)
 {
-	int	a;
-	int	b;
-	int	c;
-	
-	if (size == 2 && (*stack)->content < (*stack)->next->content)
-		move_s(stack, 'b');
-	if (size == 3)
-	{
-		a = (*stack)->content;
-		b = (*stack)->next->content;
-		c = (*stack)->next->next->content;
-		if (a > b && b < c)
-		{
-			move_r(stack, 'b');
-			move_s(stack, 'b');
-			move_rr(stack, 'b');
-			if (a < c)
-				move_s(stack, 'b');
-		}
-		else if (a < b && a < c)
-		{
-			move_s(stack, 'b');
-			move_r(stack, 'a');
-			move_s(stack, 'b');
-			move_rr(stack, 'b');
-			if (b < c)
-				move_s(stack, 'b');
-		}
-		else if (a < b && b > c && a > c)
-			move_s(stack, 'b');
-	}
-}
+	int	i;
+	int	len;
+	int	pivot;
 
+	len = ft_lstsize(*stack_b);
+	//printf("len = %i\n", len);
+	pivot = get_pivot(*stack_b, ft_lstsize(*stack_b));
+	//printf("%i\n", pivot);
+	i = 0;
+	while (i < len)
+	{
+		if ((*stack_b)->content > pivot)
+			move_pa(stack_a, stack_b);
+		else
+			move_rb(stack_b);
+		i++;
+	}
+}/*
+void	sort_up_5(t_list **stack_a, t_list **stack_b)
+{
+	while (ft_lstsize(*stack_a) > 5)
+	{
+		partition_smaller(stack_a, stack_b);
+		//print_lst(*stack_a);
+	}
+	if (!ft_lstsorted(*stack_a, 'a'))
+		sort_4_5(stack_a, stack_b);
+	while (*stack_b)
+	{
+		pull_number_up_b(stack_b, ft_lstmaximum(*stack_b));
+		move_pa(stack_a, stack_b);
+				//print_lst(*stack_b);
+
+		/*if (ft_lstsorted(*stack_b, 'b'))
+			while (*stack_b)
+				move_pa(stack_a, stack_b);
+		else
+			partition_bigger(stack_a, stack_b);
+		sort_up_5(stack_a, stack_b);
+		print_lst(*stack_a);
+
+	}
+}*/
 int	quicksort_pile_b(t_list **stack_a, t_list **stack_b, int size);
 
 int	quicksort_pile_a(t_list **stack_a, t_list **stack_b, int size)
@@ -462,16 +561,11 @@ int	quicksort_pile_a(t_list **stack_a, t_list **stack_b, int size)
 
 	pcount = 0;
 	rcount = 0;
-	if (ft_lstsorted(*stack_a, 'a'))
+	if (ft_lstsorted_a(*stack_a))
 		return (1);
-	/*if (size <= 2 && (*stack_a)->content > (*stack_a)->next->content)
+	if (size <= 2 && (*stack_a)->content > (*stack_a)->next->content)
 	{
-		move_s(stack_a, 'a');
-		return (1);
-	}*/
-	if (size <=3)
-	{
-		sort_three_top_a(stack_a, size);
+		move_sa(stack_a);
 		return (1);
 	}
 	pivot = get_pivot(*stack_a, size);
@@ -480,14 +574,13 @@ int	quicksort_pile_a(t_list **stack_a, t_list **stack_b, int size)
 		if ((*stack_a)->content < pivot && ++pcount)
 			move_pb(stack_a, stack_b);
 		else if (++rcount)
-			move_r(stack_a, 'a');
+			move_ra(stack_a);
 	while (rcount-- && size < ft_lstsize(*stack_a))
-		move_rr(stack_a, 'a');
+		move_rra(stack_a);
 	//print_lst(*stack_a);
 	//print_lst(*stack_b);
 	return (quicksort_pile_a(stack_a, stack_b, size - pcount) && quicksort_pile_b(stack_a, stack_b, pcount));
 }
-
 int	quicksort_pile_b(t_list **stack_a, t_list **stack_b, int size)
 {
 	int pcount;
@@ -498,7 +591,7 @@ int	quicksort_pile_b(t_list **stack_a, t_list **stack_b, int size)
 	rcount = 0;
 	while (stack_b)
 	{
-	if (ft_lstsorted(*stack_b, 'b'))
+	if (ft_lstsorted_b(*stack_b))
 	{
 		while (size--)
 			move_pa(stack_a, stack_b);
@@ -507,27 +600,20 @@ int	quicksort_pile_b(t_list **stack_a, t_list **stack_b, int size)
 	if (size <= 2)
 	{
 		if ((*stack_b)->content < (*stack_b)->next->content)
-			move_s(stack_b, 'b');
+			move_sb(stack_b);
 		while (pcount++ < size)
 			move_pa(stack_a, stack_b);
 		return (1);
 	}
-	/*if (size <=3)
-	{
-		sort_three_top_b(stack_b, size);
-		while (pcount++ < size)
-			move_pa(stack_a, stack_b);
-		return (1);
-	}*/
 	pivot = get_pivot(*stack_b, size);
 	//printf("pivot = %i\n", pivot);
 	while (pcount + 1 < size / 2 + size % 2)
 		if ((*stack_b)->content > pivot && ++pcount)
 			move_pa(stack_a, stack_b);
 		else if (++rcount)
-			move_r(stack_b, 'b');
+			move_rb(stack_b);
 	while (rcount-- && size - pcount < ft_lstsize(*stack_b))
-		move_rr(stack_b, 'b');
+		move_rrb(stack_b);
 	//print_lst(*stack_a);
 	//print_lst(*stack_b);
 	return (quicksort_pile_a(stack_a, stack_b, pcount) && quicksort_pile_b(stack_a, stack_b, size - pcount));
@@ -550,7 +636,7 @@ void	push_swap(int argc, char **argv)
 	if (check_arg_order(argc, argv))
 	{
 		if (argc == 3)
-			move_s(&stack_a, 'a');
+			move_sa(&stack_a);
 		else if (argc == 4)
 			sort_3(&stack_a);
 		else if (argc == 5 || argc == 6)
@@ -568,7 +654,7 @@ int	main(int argc, char **argv)
 	if (argc < 2)
 		exit (0);
 	if (check_arg_error(argc, argv))
-		write(2, "Error\n", 6);
+		write(1, "Error\n", 6);
 	else
 		push_swap(argc, argv);
 	return (0);
